ded8af8a77d69ff32225d1d6edf4adfc85b9532d
AmrelsayeD222 <amrptmode@gmail.com>
2025-11-29 23:09:24 +0200

    splash view

---
+++ lib/features/splash/splash_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../core/constants/app_colors.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primary,
+      body: Column(
+        children: [
+          SizedBox(height: MediaQuery.of(context).size.height * 0.4),
+          SvgPicture.asset('assets/splash/splash_logo.svg'),
+          const Spacer(),
+          const Image(
+            image: AssetImage('assets/splash/splash_image.png'),
+          ),
+        ],
+      ),
+    );
+  }
+}
